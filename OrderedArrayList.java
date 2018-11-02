81ced6be8d009e42aebacd962344e506cc872677
cyee00 <cyee00@stuy.edu>
2018-11-02 11:08:30 -0400

    ordered

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,12 +3,14 @@ import java.util.List;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   public OrderedArrayList(){}
   public OrderedArrayList(int startingCapacity){super(startingCapacity);}
-  public int correctIndex (T object, NoNullArrayList list) {
+  public int correctIndex (T value, NoNullArrayList<T> list) {
     int ans = 0;
     for (int i=0;i<list.size();i++) {
-
+      if (value.compareTo(list.get(i))==1){
+        return i;
+      }
     }
-    return ans;
+    return list.size()-1;
   }
   public boolean add(T object) {
     return false;
