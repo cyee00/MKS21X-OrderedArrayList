877dbe01ba92f509651af957a1eaf1991a04126e
cyee00 <cyee00@stuy.edu>
2018-11-02 10:00:39 -0400

    c

---
+++ OrderedArrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList(){}
+  public OrderedArrayList(int startingCapacity){super(startingCapacity);}
+  public int correctIndex (T object, NoNullArrayList list) {
+    int ans = 0;
+    for (int i=0;i<list.size();i++) {
+
+    }
+    return ans;
+  }
+  public boolean add(T object) {
+    return false;
+  }
+}
