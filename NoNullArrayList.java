26ce1a7ccb3f7acea5a2e4c811f02157a640f9d0
cyee00 <cyee00@stuy.edu>
2018-11-02 09:42:47 -0400

    set

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
-public class NoNullArrayList<A> extends ArrayList<A>{
+public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(){
   }
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
+  }
+  public T set(int index, T object) {
+    if (object==null){
+      throw new IllegalArgumentException();
+    } else {
+      super.set(index,object);
+    }
+    return null;
   }
 }
