a090a5445fee66f06ead5b7594e70520a3c9b181
cyee00 <cyee00@stuy.edu>
2018-11-02 09:36:38 -0400

    nonull

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 public class NoNullArrayList<A> extends ArrayList<A>{
-  public NoNullArrayList(){}
+  public NoNullArrayList(){
+  }
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
 }
